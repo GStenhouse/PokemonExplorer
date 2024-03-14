9d9b5eef28e8a4fd4bb71b6eab57390579307ef8
George Stenhouse <48562916+GStenhouse@users.noreply.github.com>
2024-03-14 16:49:57 +0000

    Update poke_explorer.py

--- poke_explorer.py
+++ poke_explorer.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import numpy as np
 import requests
 
+
 st.title("Pokemon Explorer")
 
 @st.cache_data
@@ -14,7 +15,7 @@ def get_details(poke_number):
         url = f'https://pokeapi.co/api/v2/pokemon/{poke_number}/'
         response = requests.get(url)
         pokemon = response.json()
-        return pokemon['name'], pokemon['types'][0]['type']['name'], pokemon['height'], pokemon['weight'], pokemon['moves'], pokemon['sprites']['front_default'], pokemon['cries']['latest']
+        return pokemon['name'], pokemon['types'], pokemon['height'], pokemon['weight'], pokemon['moves'], pokemon['cries']['latest'], pokemon['id'], pokemon['stats']
     except:
         return 'Error', np.NAN, np.NAN, np.NAN, np.NAN, np.NAN
 
@@ -26,13 +27,28 @@ pokemon_number = st.number_input("pick a pokemon",
                            label_visibility="hidden"
                            )
 
-name, m_type, height, weight, moves, img, cry = get_details(pokemon_number)
+name, types, height, weight, moves, cry, id, stats = get_details(pokemon_number)
+
 height = height * 10
 num_moves = len(moves)
 move_lst = [moves[i]['move']['name'] for i in range(0, len(moves))]
 moves_str = ', '.join(move_lst)
 
-height_data = pd.DataFrame({'Pokemon': ['Weedle', name, 'Onix'], 'Height': [30, height, 880]})
+stats_tbl = pd.DataFrame(data = [], columns = ['Value'])
+
+stats_tbl.loc['height'] = height
+stats_tbl.loc['weight'] = weight
+stats_tbl.loc['base hp'] = stats[0]['base_stat']
+stats_tbl.loc['base attack'] = stats[1]['base_stat']
+stats_tbl.loc['base defence'] = stats[2]['base_stat']
+stats_tbl.loc['base special attack'] = stats[3]['base_stat']
+stats_tbl.loc['base special defence'] = stats[4]['base_stat']
+stats_tbl.loc['base speed'] = stats[5]['base_stat']
+
+m_type = types[0]['type']['name']
+
+img = f'https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other/official-artwork/{id}.png?raw=true'
+shiny_img = f'https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other/official-artwork/shiny/{id}.png?raw=true'
 
 if m_type == 'grass':
    poke_colour = 'limegreen'
@@ -72,24 +88,47 @@ else:
 
 colours = ['grey', poke_colour, 'grey']
 
-graph = sns.barplot(data=height_data, 
-                    x='Pokemon', 
-                    y='Height', 
-                    palette=colours)
+
+height_data = pd.DataFrame({'Pokemon': ['Weedle', name, 'Eternatus'], 'Height': [30, height, 2000]})
+weight_data = pd.DataFrame({'Pokemon': ['Gastly', name, 'Celesteela'], 'Weight': [1, weight, 9999]})
+
+fig, (axs1, axs2) = plt.subplots(1, 2, figsize=(15, 8))
+sns.barplot(data=height_data, x='Pokemon', y='Height', palette=colours, ax=axs1)
+sns.barplot(data=weight_data, x='Pokemon', y='Weight', palette=colours, ax=axs2)
 
 # disply pokemon details
 st.subheader(f'Name: {name}')
 
 colm1, colm2 = st.columns(2)
 with colm1:
-    st.image(img, width=150)
-    st.audio(cry)
-with colm2:
-    st.write(f'Main type: {m_type}')
-    st.write(f'Height: {height}')
-    st.write(f'Weight: {weight}')
-    with st.expander(f'Moves: {num_moves}'):
-        st.write(moves_str)
+    on = st.toggle('Shiny', False)
+    if on:
+        st.image(shiny_img, width=350)
+    else:
+        st.image(img, width=350)
+    
+    for i in range(0, len(types)):
+        st.subheader(f'{types[i]["type"]["name"]} type')
 
-st.subheader("Height compared to largest and smallest pokemon")
-st.pyplot(graph.figure)
+with colm2:
+    st.audio(cry)
+    st.table(stats_tbl)
+    
+with st.expander(f'Moves: {num_moves}'):
+    st.write(moves_str)
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Height VS largest and smallest pokemon")
+
+with col2:
+    st.subheader("Weight VS heaviest and lightest pokemon")
+    
+st.pyplot(fig.figure)
+plt.close()
+
+  
+
+    
+    
+
